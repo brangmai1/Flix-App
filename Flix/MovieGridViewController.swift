43ca96a72a1ba2b7245cba71f2ed4b8f55d3f7ea
Brang Mai <brangmai100@gmail.com>
2021-09-14 23:07:43 -0400

    Added SuperheroDetailsViewController file to the project and implemented some features to the movie grid

--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -29,7 +29,6 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         let width = (view.frame.size.width - layout.minimumInteritemSpacing * rulerFive) / rulerThree
         layout.itemSize = CGSize(width: width, height: width * rulerThree / rulerTwo)
         
-        
         let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -42,9 +41,7 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
                     
                 self.movies = dataDictionary["results"] as! [[String:Any]]
                 self.collectionView.reloadData()
-                
                 print(self.movies)
-                
              }
         }
         task.resume()
@@ -56,28 +53,28 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGridCell", for: indexPath) as! MovieGridCell
-        
         let movie = movies[indexPath.item]
         let baseUrl = "https://image.tmdb.org/t/p/w500"
         let posterPath = movie["poster_path"] as! String
         let posterUrl = URL(string: baseUrl + posterPath)
-        
         cell.posterView.af_setImage(withURL: posterUrl!)
-        
         return cell 
     }
     
-    
-    
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-    }
-    */
+        
+        // Find the selected movie
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
 
+        // Pass the selected movie to the details view controller
+        let detailsGridViewController = segue.destination as! SuperheroDetailsViewController
+        detailsGridViewController.movie = movie
+    }
 }
--- Flix/SuperheroDetailsViewController.swift
+++ Flix/SuperheroDetailsViewController.swift
@@ -1,43 +1,42 @@
 //
-//  MovieDetailsViewController.swift
+//  SuperheroDetailsViewController.swift
 //  Flix
 //
-//  Created by Brang Mai on 9/11/21.
+//  Created by Brang Mai on 9/13/21.
 //
 
 import UIKit
 import AlamofireImage
 
-class MovieDetailsViewController: UIViewController {
-    
-    
+class SuperheroDetailsViewController: UIViewController {
+
     @IBOutlet weak var backdropView: UIImageView!
     @IBOutlet weak var posterView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var synopsisLabel: UILabel!
     
     var movie : [String: Any]!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         
+        // set up the view.
         let backdropPath = movie["backdrop_path"] as! String
         let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
         backdropView.af_setImage(withURL: backdropUrl!)
-        
+
         let baseUrl = "https://image.tmdb.org/t/p/w500"
         let posterPath = movie["poster_path"] as! String
         let posterUrl = URL(string: baseUrl + posterPath)
         posterView.af_setImage(withURL: posterUrl!)
-        
+
         titleLabel.text = movie["title"] as? String
         titleLabel.sizeToFit()
+        
         synopsisLabel.text = movie["overview"] as? String
         synopsisLabel.sizeToFit()
+        
     }
-    
 
     /*
     // MARK: - Navigation
